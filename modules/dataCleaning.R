f61a6b56c02f5b245a527a110c941d5ab9a6bc49
Juan José Díaz <jj.diaz1067@uniandes.edu.co>
2016-10-07 17:31:05 -0500

    Format of lines

--- modules/dataCleaning.R
+++ modules/dataCleaning.R
@@ -8,29 +8,30 @@ dataCleaningUI <- function(id){
                   choices = list("PM2.5" = 1, "PM10" = 2), 
                   selected = 1),
       hr(),
-      checkboxGroupInput(ns("checkGroup1"), label = h3("Reglas generales"), 
-                         choices = list("Restriccion de cadenas de texto" = 1, "Valores negativos y ceros" = 2, "Limite de detección del equipo" = 3),
+      checkboxGroupInput(ns("generalRules"), label = h3("Reglas generales"), 
+                         choices = list("Restriccion de cadenas de texto" = 1, "Valores negativos y ceros" = 2, "Limite de detección del equipo" = 3)
                          ),
-      checkboxGroupInput(ns("checkGroup2"), label = h3("Reglas particulares a PM2.5"), 
+      checkboxGroupInput(ns("particularRules"), label = h3("Reglas particulares a PM2.5"), 
                          choices = list("Asegurar PM10 > PM2.5" = 1)
       ),
-      checkboxGroupInput(ns("checkGroup3"), label = h3("Reglas de densidad de datos"), 
+      checkboxGroupInput(ns("densityRules"), label = h3("Reglas de densidad de datos"), 
                          choices = list("Eliminar datos aislados" = 1)
-      ),
+      )
       
     ),
     mainPanel(
-      
+      uiOutput(ns("summary"))
     )
   )
 }
+
+# dataBasePM2.5 = read.csv("PM2.5_1998_2014_Encsv.csv", sep=";", row.names=NULL, stringsAsFactors=TRUE)
 dataCleaning <- function(input, output, session){
-  
   datasetToUse <- reactive({
     
   })
   
-  output$setOfRules <- renderUI({
+  output$summary <- renderUI({
     
   })
 }
