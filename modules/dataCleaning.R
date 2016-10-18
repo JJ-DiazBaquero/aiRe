47d647cfe8d7aa96ca02e8a4a621c116a0eee1d7
Juan José Díaz <jj.diaz1067@uniandes.edu.co>
2016-10-18 17:55:14 -0500

    Succesful implementation of rule 1 in an acceptable computational time

--- modules/dataCleaning.R
+++ modules/dataCleaning.R
@@ -10,15 +10,17 @@ dataCleaningUI <- function(id){
                   selected = 1),
       hr(),
       checkboxGroupInput(ns("generalRules"), label = h3("Reglas generales"), 
-                         choices = list("Restriccion de cadenas de texto" = 1, "Valores negativos y ceros" = 2, "Limite de detección del equipo" = 3)
+                         choices = list("Restriccion de cadenas de texto" = 1, 
+                                        "Valores negativos y ceros" = 2, 
+                                        "Limite de detección del equipo" = 3)
                          ),
       checkboxGroupInput(ns("particularRules"), label = h3("Reglas particulares a PM2.5"), 
                          choices = list("Asegurar PM10 > PM2.5" = 1)
       ),
       checkboxGroupInput(ns("densityRules"), label = h3("Reglas de densidad de datos"), 
                          choices = list("Eliminar datos aislados" = 1)
-      )
-      
+      ),
+      actionButton(ns("applyRulesBtn"), "Aplicar reglas")
     ),
     mainPanel(
       tableOutput(ns("rulesTable")),
@@ -33,28 +35,52 @@ dataCleaning <- function(input, output, session, database){
   for(i in 1:6){
     rulesSummarydf[paste("Regla ",i)] = rep(0,11)
   }
-  rulesSummary <- reactiveValues(data = rulesSummarydf)
+  rulesSummary <- reactiveValues(data = rulesSummarydf, 
+                                 rulesMatrix = matrix(0,nrow = nrow(database), 
+                                                ncol=length(database)-1))
   output$prueba <- renderText({
     return(1 %in% input$generalRules)
   })
   cleanData <- reactive({
-    cat(file=stderr(),"cambia valor reglas generales")
-    rules = input$generalRules
-    if(1 %in% rules){
+    input$applyRulesBtn
+    if(1 %in% input$generalRules){
       #Encontrar todos los valores de string diferentes en la columna para asi quedar con solo números
-      for(i in 2:12){
+      for(i in 2:length(database)){
         lvlsStr = levels(database[,i])
         lvlsInt = as.numeric(lvlsStr)
         strList = lvlsStr[is.na(lvlsInt)]
-        database[ database %in% strList] == 0
+        rulesSummary$rulesMatrix[database[,i] %in% strList] == 1
+        database[database[,i] %in% strList] == 0
         database[,i] = as.numeric(database[,i])
         cat("cambiando columna ", i)
-        
       }
+      rulesSummary$data[1,] = colSums(rulesSummary$rulesMatrix)
     }
   })
-  output$rulesTable <-renderTable(
-    rulesSummary$data, striped = TRUE)
+  output$rulesTable <-renderTable({
+    
+    input$applyRulesBtn
+    if(1 %in% input$generalRules){
+      #Encontrar todos los valores de string diferentes en la columna para asi quedar con solo números
+      rule1Array = c(rep(0,nrow(database)))
+      for(i in 2:length(database)){
+        lvlsStr = levels(database[,i])
+        lvlsInt = as.numeric(lvlsStr)
+        strList = lvlsStr[is.na(lvlsInt)]
+        rule1Array[database[,i] %in% strList] = 1
+        database[,i] = as.numeric(database[,i])
+        database[database[,i] %in% strList,] = 0
+        cat("cambiando columna ", i, "\n")
+        cat("Numero de entradas con string ", sum(rule1Array), "\n")
+        rulesSummary$data[i-1,2] = sum(rule1Array)/nrow(lel)
+        rulesSummary$rulesMatrix[,i-1] = rule1Array
+        rule1Array[TRUE]=0
+      }
+      cat("Suma de columnas: \n")
+      cat(colSums(rulesSummary$rulesMatrix))
+    }
+    
+    return(rulesSummary$data)}, striped = TRUE)
   output$summary = renderPrint({
     summary(database[,2:12])
   })
