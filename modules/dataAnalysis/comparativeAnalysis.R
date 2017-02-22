d85fcdb9ac87817264b0665fcec72577d207d577
Juan José Díaz <jj.diaz1067@uniandes.edu.co>
2017-02-22 14:59:08 -0500

    Added first special dates csv format Change the order of comparative analysis, the UI related method in server are in the low part of the R document Added UI por special dates dateranges Added Logic por loading special date ranges

--- modules/dataAnalysis/comparativeAnalysis.R
+++ modules/dataAnalysis/comparativeAnalysis.R
@@ -2,7 +2,17 @@ comparativeAnalysisUI <- function(id) {
   ns <- NS(id)
   navbarPage(
     "Analisis comparativo",
-    tabPanel("Parametros",
+    tabPanel("Fechas especiales",
+             selectInput(ns("selectedSpecialDate"), label = "Seleccione evento a analizar:",
+                         choices = list("Dia sin carro" = 0), 
+                         selected = 0),
+             sliderInput(ns("specialDateYears"), "Anios:",
+                         min = 2008, max = 2014, value = c(2008,2014)),
+             sliderInput(ns("specialDateHours"), "Hora:",
+                         min = 0, max = 23, value = c(0,23)),
+             actionButton(ns("specialDateSubmit"),"Enviar cambios")
+    ),
+    tabPanel("Fechas Personalizadas",
              fluidRow(
                column(4,
              h3("Rango de fechas 1"),
@@ -64,6 +74,116 @@ comparativeAnalysis <- function(input, output, session, database) {
   #intervalData is the informacion of the data contain in each range of dates
   reactiveData <- reactiveValues(intervalData = list())
   
+  output$numObsTable <- renderTable({
+    reactiveData$intervalData
+    isolate({
+      obsTable = data.frame(Estacion = names(database[['data']]), row.names = "Estacion")
+      row.names(obsTable)[1] = "Maximo de observaciones (maximo teorico)"
+      for(i in 1:3){
+        if(input[[paste("use",i,sep="")]] == TRUE){
+          obsTable[paste("Rango",i)] = colSums(!is.na(reactiveData$intervalData[[i]]))
+        }
+      }
+      #Check if the dataframe is empty, this avoid crash when there is no range dates selected (no columns in dt)
+      if(is.na(colnames(obsTable)[1])){
+        obsTable = NULL
+      }
+      obsTable
+    })
+  },
+  caption = "Numero de observaciones en cada estacion y rango de fecha", 
+  rownames = TRUE, colnames = TRUE, striped = TRUE, bordered = TRUE
+  )
+  
+  #This function search for the data in each personalizeddate range
+  calcAvailavility<- observe({
+    input$makeGraphs
+    isolate({
+      data = list()
+      for(i in 1:3){
+        if(input[[paste("use",i,sep="")]] == TRUE){
+          #The user chooses by number of the day
+          if(input[[paste("typeOfWeek",i,sep="")]] == 1){
+            #get Number of day
+            days = seq(from = input[[paste("rangeDay",i,sep="")]][1], to = input[[paste("rangeDay",i,sep="")]][2])
+            dates = data.frame(date = database[['data']][,1])
+            dates$day = as.numeric(format(dates$date, "%d"))
+            dates = dates[dates$day %in% days,]
+          }
+          #The user chooses by weekday
+          if(input[[paste("typeOfWeek",i,sep="")]] == 0){
+            #get weekdays selected by user
+            #The functionality of this change on the language of the machine (originally done in spanish)
+            dates = data.frame(date = database[['data']][,1])
+            dates = cutData(dates, type = "weekday", start.day = 1)
+            dates$weekday = iconv(dates$weekday,to="ASCII//TRANSLIT")
+            dates = dates[dates$weekday %in% input[[paste("rangeDay",i,sep="")]],]
+          }
+          
+          #Get the hours of interest (the +1 is for the numeric transformation of openAir factor hour)
+          hours = seq(from = input[[paste("rangeHour",i,sep="")]][1]+1, to = input[[paste("rangeHour",i,sep="")]][2]+1)
+          dates = cutData(dates, type = "hour")
+          dates = dates[as.numeric(dates$hour) %in% hours,]
+          
+          #Get the months of interest
+          months = seq(from = input[[paste("rangeMonth",i,sep="")]][1], to = input[[paste("rangeMonth",i,sep="")]][2])
+          dates$month = as.numeric(format(dates$date, "%m"))
+          dates = dates[dates$month %in% months,]
+          
+          #Get the years of interest
+          years = seq(from = input[[paste("rangeYear",i,sep="")]][1], to = input[[paste("rangeYear",i,sep="")]][2])
+          dates$year = as.numeric(format(dates$date, "%Y"))
+          dates = dates[dates$year %in% years,]
+          reactiveData$intervalData[[i]] = database[['data']][database[['data']][,1] %in% dates$date,]
+        }
+      }
+    })
+  })
+  
+  #This function search for the data in the selected special date
+  calcSpecialDate <- observe({
+    input$specialDateSubmit
+    isolate({
+      data = read.csv("databases/SpecialDates.csv", sep=";", row.names=NULL, stringsAsFactors=TRUE)
+      data = as.POSIXct(as.character(data), format="%d/%m/%Y")
+    })
+  })
+  
+  #This function build the boxplot with the information in each selected range
+  #The "stack" method is the lenghtiest operation in this method
+  #TODO search another way to build boxplot without using "stack" and keep same behaviour
+  output$boxplot <- renderPlotly({
+    p = plot_ly(x = input$stations, type = "box", visible = FALSE)
+    for(i in 1:3){
+      if(input[[paste("use",i,sep="")]] == TRUE){
+        info = reactiveData$intervalData[[i]]
+        info = stack(info, select = input$stations)
+        p = add_trace(p, y = info$values, x = info$ind, 
+                      name = input[[paste("nameRange",i,sep="")]], visible = TRUE)
+      }
+    }
+    p = layout(p,boxmode = "group")
+    p
+  })
+  
+  #output$scatterplot <- renderPlotly({
+  #  p = plot_ly(x = input$stations, type = "scatter", visible = FALSE)
+  #  for(i in 1:3){
+  #    if(input[[paste("use",i,sep="")]] == TRUE){
+  #      info = reactiveData$intervalData[[i]]
+  #      info = stack(info, select = input$stations)
+  #      p = add_trace(dMeans, x = ~ind, y = ~values, 
+  #                    type = "scatter", error_y = list(value = dsd$values),
+  #                    visible = TRUE)
+  #    }
+  #  }
+  #  p = layout(p,scattermode = "group")
+  #  p
+  #})
+  
+  #--------------------------------------------------------------------
+  # Functions for filling UI Components
+  #--------------------------------------------------------------------
   output$weekui1 <- renderUI({
     if(input$typeOfWeek1 == 0){
       checkboxGroupInput(ns("rangeDay1"), "Dias de la semana:",
@@ -129,98 +249,5 @@ comparativeAnalysis <- function(input, output, session, database) {
     )
   })
   
-  output$numObsTable <- renderTable({
-    reactiveData$intervalData
-    isolate({
-      obsTable = data.frame(Estacion = names(database[['data']]), row.names = "Estacion")
-      row.names(obsTable)[1] = "Maximo de observaciones (maximo teorico)"
-      for(i in 1:3){
-        if(input[[paste("use",i,sep="")]] == TRUE){
-          obsTable[paste("Rango",i)] = colSums(!is.na(reactiveData$intervalData[[i]]))
-        }
-      }
-      #Check if the dataframe is empty, this avoid crash when there is no range dates selected (no columns in dt)
-      if(is.na(colnames(obsTable)[1])){
-        obsTable = NULL
-      }
-      obsTable
-    })
-  },
-  caption = "Numero de observaciones en cada estacion y rango de fecha", 
-  rownames = TRUE, colnames = TRUE, striped = TRUE, bordered = TRUE
-  )
-  
-  calcAvailavility<- observe({
-    input$makeGraphs
-    isolate({
-      data = list()
-      for(i in 1:3){
-        if(input[[paste("use",i,sep="")]] == TRUE){
-          #The user chooses by number of the day
-          if(input[[paste("typeOfWeek",i,sep="")]] == 1){
-            #get Number of day
-            days = seq(from = input[[paste("rangeDay",i,sep="")]][1], to = input[[paste("rangeDay",i,sep="")]][2])
-            dates = data.frame(date = database[['data']][,1])
-            dates$day = as.numeric(format(dates$date, "%d"))
-            dates = dates[dates$day %in% days,]
-          }
-          #The user chooses by weekday
-          if(input[[paste("typeOfWeek",i,sep="")]] == 0){
-            #get weekdays selected by user
-            #The functionality of this change on the language of the machine (originally done in spanish)
-            dates = data.frame(date = database[['data']][,1])
-            dates = cutData(dates, type = "weekday", start.day = 1)
-            dates$weekday = iconv(dates$weekday,to="ASCII//TRANSLIT")
-            dates = dates[dates$weekday %in% input[[paste("rangeDay",i,sep="")]],]
-          }
-          
-          #Get the hours of interest (the +1 is for the numeric transformation of openAir factor hour)
-          hours = seq(from = input[[paste("rangeHour",i,sep="")]][1]+1, to = input[[paste("rangeHour",i,sep="")]][2]+1)
-          dates = cutData(dates, type = "hour")
-          dates = dates[as.numeric(dates$hour) %in% hours,]
-          
-          #Get the months of interest
-          months = seq(from = input[[paste("rangeMonth",i,sep="")]][1], to = input[[paste("rangeMonth",i,sep="")]][2])
-          dates$month = as.numeric(format(dates$date, "%m"))
-          dates = dates[dates$month %in% months,]
-          
-          #Get the years of interest
-          years = seq(from = input[[paste("rangeYear",i,sep="")]][1], to = input[[paste("rangeYear",i,sep="")]][2])
-          dates$year = as.numeric(format(dates$date, "%Y"))
-          dates = dates[dates$year %in% years,]
-          reactiveData$intervalData[[i]] = database[['data']][database[['data']][,1] %in% dates$date,]
-        }
-      }
-    })
-  })
-  
-  output$boxplot <- renderPlotly({
-    p = plot_ly(x = input$stations, type = "box", visible = FALSE)
-    for(i in 1:3){
-      if(input[[paste("use",i,sep="")]] == TRUE){
-        info = reactiveData$intervalData[[i]]
-        info = stack(info, select = input$stations)
-        p = add_trace(p, y = info$values, x = info$ind, 
-                      name = input[[paste("nameRange",i,sep="")]], visible = TRUE)
-      }
-    }
-    p = layout(p,boxmode = "group")
-    p
-  })
-  
-  #output$scatterplot <- renderPlotly({
-  #  p = plot_ly(x = input$stations, type = "scatter", visible = FALSE)
-  #  for(i in 1:3){
-  #    if(input[[paste("use",i,sep="")]] == TRUE){
-  #      info = reactiveData$intervalData[[i]]
-  #      info = stack(info, select = input$stations)
-  #      p = add_trace(dMeans, x = ~ind, y = ~values, 
-  #                    type = "scatter", error_y = list(value = dsd$values),
-  #                    visible = TRUE)
-  #    }
-  #  }
-  #  p = layout(p,scattermode = "group")
-  #  p
-  #})
   
 }
