b99cc59ed30774f5bd6c195a667c3646b33eadde
Juan José Díaz <jj.diaz1067@uniandes.edu.co>
2017-06-07 20:03:06 -0500

    Added support for a new data format

--- modules/dataLoading.R
+++ modules/dataLoading.R
@@ -5,11 +5,15 @@ dataLoadingUI <- function(id, label = "Data Loading") {
   navbarPage(
     "Reporte de datos",
     tabPanel("Carga de datos",
+             actionButton(ns('Delete'),"Borrar Actual"),
              radioButtons(ns("desiredFormat"), "Por favor elija el formato de sus datos",
                           choices = c("Documento por contaminante" = 1,"Documento por estacion" = 2 ),
                           selected = 1, inline = FALSE),
-             fileInput(ns('file'), 'Por suba un archivo CSV',
-                       accept=c('text/csv','.csv'))),
+             textInput(ns('StationName'), "Nombre de la estacion"),
+             fileInput(ns('file'), 'Por favor suba un archivo CSV',
+                       accept=c('text/csv','.csv')),
+             selectInput(ns('database'), "Seleccione contaminante", c( "PM2.5" =2,"PM10"=1)),
+             actionButton(ns('add'),"Agregar")),
     tabPanel("Visualizacion de datos",
              selectInput(ns("dataBase"), label = h3("Seleccione una base datos"), 
                          choices = list("Ninguna" = 0, "PM2.5" = 1, "PM10" = 2), 
@@ -21,35 +25,50 @@ dataLoadingUI <- function(id, label = "Data Loading") {
 
 dataLoading <- function(input, output, session) {
   
+  newDatabase <- observe({
+    input$Delete
+    isolate({
+      if(input$Delete>=1){
+    database$datapm10 = data.frame(Fecha...Hora = character())
+    database$datapm2.5 = data.frame(Fecha...Hora = character())
+    database$data = NULL
+      }
+    })
+  })
+  
   database <- reactiveValues(datapm10 = read.csv("databases/PM10_1998_2016_Encsv.csv", sep=";", row.names=NULL, stringsAsFactors=TRUE),
                              datapm2.5 = read.csv("databases/PM2.5_1998_2016_Encsv.csv", sep=";", row.names=NULL, stringsAsFactors=TRUE),
                              data = NULL)
   database[['data']] <- database[['datapm2.5']]
   
-  changeDatabse <- observe({
-    input$desiredFormat
-    #isolate({
-      #if(input$desiredFormat == 1){
-      #  database$datapm10 = read.csv("databases/PM10_1998_2014_Encsv.csv", sep=";", row.names=NULL, stringsAsFactors=TRUE)
-      #  database$datapm2.5 = read.csv("databases/PM2.5_1998_2014_Encsv.csv", sep=";", row.names=NULL, stringsAsFactors=TRUE)
-      #  database$data = NULL
-      #  database[['data']] <- database[['datapm2.5']]
-      #}
-      #else if(input$desiredFormat == 2){
-      #  cat("Cambiando formato")
-      #  folder = "databases/EjemploManyCSVCali"
-      #  file_names <- dir(folder)
-      #  
-      #  dates = as.POSIXct(character())
-      #  for (i in file_names){
-      #    data = read.csv(file = paste(folder,file_names[i], sep="/"), 
-      #                    sep=";", row.names=NULL, stringsAsFactors=FALSE)
-      #    if (!as.POSIXct(as.character(data[,1])) %in% dates){
-      #      
-      #    }
-      #  }
-      #}
-    #})
+  changeDatabase <- observe({
+    input$add
+    isolate({
+      #Document per contaminant
+      if(input$desiredFormat == 1){
+        #TODO
+      }
+      if(input$desiredFormat == 2){
+        file <- input$file
+        if (is.null(file)){
+          return(NULL)
+        }
+        data = read.csv(file$datapath,sep = ";", stringsAsFactors = F)
+        if(input$database == 1){
+          newData = as.list(database$datapm10)
+          newData["Fecha...Hora"] = as.POSIXct(as.character(data[,1]), format="%m/%d/%Y %H:%M")
+          newData[input$StationName] = data['PM10']
+          database$datapm10 = as.data.frame(newData)
+        }
+        if(input$database == 2){
+          newData = as.list(database$datapm2.5)
+          newData["Fecha...Hora"] = as.POSIXct(as.character(data[,1]), format="%d/%m/%Y %H:%M")
+          newData[input$StationName] = data['PM2.5']
+          database$datapm2.5 = as.data.frame(newData)
+        }
+      }
+    })
+    
   })
   
   changeDates <- observe({
